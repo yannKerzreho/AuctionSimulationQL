946d2694f595a9e01ae1b9a95a90be02b59b05cb
yannKerzreho <kerzreho.yann@gmail.com>
2023-12-04 20:10:37 +0100

    Initial Commit

---
+++ AuctionSimulationQL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AuctionSimulationQL
+//
+//  Created by Yann Kerzreho on 04/12/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
