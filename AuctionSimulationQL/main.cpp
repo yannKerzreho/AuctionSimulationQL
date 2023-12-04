2cfe6a99cb95831ec95374a588bc42e1702032ed
yannKerzreho <kerzreho.yann@gmail.com>
2023-12-04 21:49:00 +0100

    Starting main

--- AuctionSimulationQL/main.cpp
+++ AuctionSimulationQL/main.cpp
@@ -7,8 +7,24 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    
+    // Define possible_bet
+    std::vector<double> possible_bet;
+    double step = 0.05;
+    for (double value = 0.05; value < 1.0; value += step) {
+        possible_bet.push_back(value);
+    }
+    
+    // Define parameters
+    double alpha = 0.05;
+    double beta = 0;
+    double gamma = 0.99;
+    double epsilon = 0.025;
+    int num_it = 1000000;
+    double auction_type = 1;  // 1=>FP & 2=>SP
+
+    
+    
+    
 }
